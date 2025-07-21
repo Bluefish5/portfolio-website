7a11c0ceb3170207ac8b6813af5cf034f32ae4fa
Rafal <60304472+Bluefish5@users.noreply.github.com>
2025-07-21 19:20:27 +0200

    Icon change

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={`antialiased text-black paper-background paper-texture`}>
         <div className="min-h-screen flex flex-col">
           {/* Header */}
